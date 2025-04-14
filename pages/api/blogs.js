c78526421c9ac6eaeac97eba760fd649ccdd244a
Rahul Mondal <rahulml2505@gmail.com>
2025-04-14 20:02:59 +0530

    fix files reading - 7

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,16 +2,26 @@
 const fs = require("fs/promises");
 const path = require("path");
 
-export default async (_req, res) => {
-  const filenames = [
-    "how-to-be-a-coder.json",
-    "how-to-learn-flask.json",
-    "how-to-learn-nextjs.json",
-    "how-to-learn-unity-3d.json",
-    "how-to-learn-django.json",
-    "how-to-learn-javascript.json",
-    "how-to-learn-python.json",
-  ];
+export default async (req, res) => {
+  let filenames = [
+      "how-to-be-a-coder.json",
+      "how-to-learn-flask.json",
+      "how-to-learn-nextjs.json",
+      "how-to-learn-unity-3d.json",
+      "how-to-learn-django.json",
+      "how-to-learn-javascript.json",
+      "how-to-learn-python.json",
+    ],
+    total_blogs = filenames.length,
+    file_extension = ".json",
+    blogs_count = 3,
+    blogs = [];
+
+  if (req.query.count) {
+    blogs_count = parseInt(req.query.count);
+  }
+
+  filenames = filenames.slice(0, blogs_count);
   /***
    * ```|_ Do not use forEach with async-await _|```
    * Fortunately if your language has async-await then it will also have the `for...of` construction, so you can use that */
